37e92bc2016725039f93b0417095f8993c6a56c2
SebiGa <gauggelsebastian@yahoo.de>
2019-12-11 11:02:06 +0100

    Make that random-value is wirklich random-value.

--- Testat/scripts/moisturemap.py
+++ Testat/scripts/moisturemap.py
@@ -17,14 +17,14 @@ min = 10.0
 
 
 scaling = 0.002
-offset = random.random()
+offset = random.random()*xRange
 
 # iterate through all pixels
 pixels = [None] * xRange * yRange
 for x in range(xRange):
     for y in range(yRange):
         # perlin noise
-        pixelVec[x, y] = mathutils.noise.hetero_terrain([x*scaling+offset, y*scaling+offset, 0], 1.0, 1.0, 1, 0, noise_basis='PERLIN_ORIGINAL')
+        pixelVec[x, y] = mathutils.noise.hetero_terrain([(x+offset)*scaling, (y+offset)*scaling, 0], 1.0, 1.0, 1, 0, noise_basis='PERLIN_ORIGINAL')
         # check if value is greater than max or lesser than min
         if pixelVec[x, y] > max:
             max = pixelVec[x, y]
